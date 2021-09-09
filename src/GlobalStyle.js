a71ce8385d492c89915585bee737ca16ecb9b05d
Caner Demir <canerdemir62@gmail.com>
2021-09-08 21:54:04 -0700

    Change the app structure

---
+++ src/GlobalStyle.js
@@ -0,0 +1,96 @@
+import { createGlobalStyle } from 'styled-components';
+
+// css-reset rules
+const GlobalStyle = createGlobalStyle`
+ /** Montserrat Regular **/
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 400;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Regular.woff2") format("woff2"),
+ url("/fonts/Montserrat-Regular.woff") format("woff");
+  }
+
+  /** Montserrat Medium **/
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 500;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Medium.woff2") format("woff2"),
+ url("/fonts/Montserrat-Medium.woff") format("woff");
+  }
+
+  /** Montserrat SemiBold **/
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 600;
+    font-style: normal;
+    src: url("/fonts/Montserrat-SemiBold.woff2") format("woff2"),
+ url("/fonts/Montserrat-SemiBold.woff") format("woff");
+  }
+
+  /** Montserrat Bold **/
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 700;
+    font-style: normal;
+    src: url("Montserrat-Bold.woff2") format("woff2"),
+ url("Montserrat-Bold.woff") format("woff");
+  }
+
+  /** Bitter Regular **/
+  @font-face {
+    font-family: "Bitter";
+    src: url("/fonts/Bitter-Regular.woff2") format("woff2"),
+ url("/fonts/Bitter-Regular.woff") format("woff");
+  }
+
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    font-family: ${(props) => props.theme.font.family.default};
+    font-size: ${(props) => props.theme.font.size.default};
+    line-height: ${(props) => props.theme.font.lineHeight.default};
+    color: ${(props) => props.theme.color.text};
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+`;
+
+export default GlobalStyle;
